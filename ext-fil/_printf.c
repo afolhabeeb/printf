193f09b72a1bf1f601b8085c52baa315dee06718
Habeeb Afolabi ojedokun <ojefolabi@yahoo.com>
2023-11-14 03:11:09 -0800

    leggo again

--- ext-fil/_printf.c
+++ ext-fil/_printf.c
@@ -1,55 +1,48 @@
 #include "main.h"
 
 /**
- * _printf - prints to standard output formatted text
+ * _printf - custom printf
+ * @format: identifier look for
  *
- * @format: pointer to printed  format.
- *
- * Return: printf
+ * Return: integer
  */
 
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-	unsigned int count = 0;
+	convert p[] = {
+		{"%s", printf_string}, {"%c", print_char},
+		{"%%", print_per},
+		{"%i", printf_int}, {"%d", print_dec},
+		{"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_str}, {"%p", print_pointer}
+	};
+
 	va_list args;
+	int i = 0, lens = 0;
+	int j;
 
 	va_start(args, format);
-	while (*format != '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+Here:
+	while (format[i] != '\0')
 	{
-		if (*format == '%')
+		j = 13;
+		while (j >= 0)
 		{
-			format++;
-			switch (*format)
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
 			{
-				case 'c':
-					char c = va_arg(args, int);
-
-					count += write(1, &c, 1);
-					break;
-				case 's':
-					char *str = va_arg(args, char*);
-					int len_str = 0;
-
-					while (str[len_str] != '\0')
-						len_str++;
-					count += write(1, str, len_str);
-					break;
-				case '%':
-					write(1, format, 1);
-					count++;
-					break;
-				default:
-					break;
+				lens += p[j].function(args);
+				i = i + 2;
+				goto Here;
 			}
-			format++;
+			j--;
 		}
-		else
-		{
-			write(1, format, 1);
-			count++;
-			format++;
-		}
+		putchar_prt(format[i]);
+		lens++;
+		i++;
 	}
 	va_end(args);
-	return (count);
+	return (lens);
 }
